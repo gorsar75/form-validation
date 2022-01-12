315d71f6e7259fd4737b321d0df98e38330869bd
gorsar75 <92424358+gorsar75@users.noreply.github.com>
2022-01-11 20:28:55 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(selectors: '#form');
+const nameInput = document.querySelector('#name');
+const results = document.querySelector('#results');
+
+console.log(user.name)
+
+applicationBtn.addEventListener('click',function () {
+    const aName = prompt("Enter your name");
+}
+
+form.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log(nameInput.value);
+    console.log(aName);
+   
+
+})
+
+const form = document.querySelector( selectors: '#form');
+const emailInput = document.querySelector('#email');
+const results = document.querySelector('#results');
+
+console.log(user.email)
+
+applicationBtn.addEventListener('click',function () {
+    const anEmail = prompt("Enter your email");
+})
